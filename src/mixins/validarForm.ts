93a1f913ded17e3d39faa9143ee698110ad17bfe
GUILHERME CASTELO <guilhermecastelo.mail@gmail.com>
2022-03-24 10:50:05 -0400

    refatoramento das funcoes

--- src/mixins/validarForm.ts
+++ src/mixins/validarForm.ts
@@ -1,84 +1,53 @@
-const validarFormMixin = {
+import { tipoNotificacao } from "@/interfaces/INotificacao";
+import { notificacaoMixin as n } from "@/mixins/notificar";
+
+const validarForm = {
   methods: {
-    removeClass(item: string, obj: any, classe: string) {
-      while (!obj) {
-        return;
+    validaCnpjCpf(cnpjCpf: string): string {
+      cnpjCpf = cnpjCpf.replace(/\D/g, "");
+      if (cnpjCpf.length <= 11) {
+        cnpjCpf = this.maskCpf(cnpjCpf);
+      } else if (cnpjCpf.length <= 14) {
+        cnpjCpf = this.maskCnpj(cnpjCpf);
       }
-      return document.querySelector(item)?.classList.remove(classe);
+      return cnpjCpf
     },
-    addClass(item: string, classe: string) {
-      return document.querySelector(item)?.classList.add(classe);
+    validaTipoCli(cnpjCpf: string): string {
+      if (cnpjCpf.length <= 14) {
+        return "PF";
+      } else if (cnpjCpf.length <= 18) {
+        return "PJ";
+      }
+      return "";
     },
-    validaCpf(cpf: string) {
-      if (
-        !cpf ||
-        cpf === "00000000000" ||
-        cpf === "00000000000" ||
-        cpf === "11111111111" ||
-        cpf === "22222222222" ||
-        cpf === "33333333333" ||
-        cpf === "44444444444" ||
-        cpf === "55555555555" ||
-        cpf === "66666666666" ||
-        cpf === "77777777777" ||
-        cpf === "88888888888" ||
-        cpf === "99999999999"
-      )
+    maskCnpj(cnpj: string): string {
+      cnpj = cnpj.replace(/\D/g, "");
+      cnpj = cnpj.replace(/^(\d{2})(\d)/, "$1.$2");
+      cnpj = cnpj.replace(/^(\d{2})\.(\d{3})(\d)/, "$1.$2.$3");
+      cnpj = cnpj.replace(/\.(\d{3})(\d)/, ".$1/$2");
+      cnpj = cnpj.replace(/(\d{4})(\d)/, "$1-$2");
+      return cnpj;
+    },
+    maskCpf(cpf: string): string {
+      cpf = cpf.replace(/\D/g, "");
+      cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2");
+      cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2");
+      cpf = cpf.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+      return cpf;
+    },
+    validarNulo(campo: string) {
+      if (campo != "") {
         return false;
-      let soma = 0;
-      let resto;
-      for (let i = 1; i <= 9; i++)
-        soma = soma + parseInt(cpf.substring(i - 1, i)) * (11 - i);
-      resto = (soma * 10) % 11;
-      if (resto == 10 || resto == 11) resto = 0;
-      if (resto != parseInt(cpf.substring(9, 10))) return false;
-      soma = 0;
-      for (let i = 1; i <= 10; i++)
-        soma = soma + parseInt(cpf.substring(i - 1, i)) * (12 - i);
-      resto = (soma * 10) % 11;
-      if (resto == 10 || resto == 11) resto = 0;
-      if (resto != parseInt(cpf.substring(10, 11))) return false;
-      return true;
-    },
-    validaCNPJ(cnpj: any) {
-      if (
-        !cnpj ||
-        cnpj == "00000000000000" ||
-        cnpj == "11111111111111" ||
-        cnpj == "22222222222222" ||
-        cnpj == "33333333333333" ||
-        cnpj == "44444444444444" ||
-        cnpj == "55555555555555" ||
-        cnpj == "66666666666666" ||
-        cnpj == "77777777777777" ||
-        cnpj == "88888888888888" ||
-        cnpj == "99999999999999"
-      )
-        return false;
-      let tamanho = cnpj.length - 2;
-      let numeros = cnpj.substring(0, tamanho);
-      const digitos = cnpj.substring(tamanho);
-      let soma = 0;
-      let pos = tamanho - 7;
-      for (let i = tamanho; i >= 1; i--) {
-        soma += numeros.charAt(tamanho - i) * pos--;
-        if (pos < 2) pos = 9;
+      } else {
+        n.methods.notificar(
+          tipoNotificacao.ATENCAO,
+          "Verifique",
+          "o campo " + campo + " nao pode ser nulo ou vazio"
+        );
+        return true;
       }
-      let resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
-      if (resultado != digitos.charAt(0)) return false;
-      tamanho = tamanho + 1;
-      numeros = cnpj.substring(0, tamanho);
-      soma = 0;
-      pos = tamanho - 7;
-      for (let i = tamanho; i >= 1; i--) {
-        soma += numeros.charAt(tamanho - i) * pos--;
-        if (pos < 2) pos = 9;
-      }
-      resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
-      if (resultado != digitos.charAt(1)) return false;
-      return true;
     },
   },
 };
 
-export default validarFormMixin;
+export default validarForm;
