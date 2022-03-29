9c432d0e3912a671772defc6c282a9b7741a27a2
GUILHERME CASTELO <guilhermecastelo.mail@gmail.com>
2022-03-29 17:38:37 -0400

    add button addReference

---
+++ src/components/LimitePrazo/Referencias/AddReference.vue
@@ -0,0 +1,13 @@
+<template>
+  <Button icon="fa-plus" name="Adicionar" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Button from "@/components/Button.vue";
+
+export default defineComponent({
+  name: "AddReference",
+  components: { Button },
+});
+</script>
