9777544610ddeb72e69b0aae70a82db2c93a98a0
GUILHERME CASTELO <guilhermecastelo.mail@gmail.com>
2022-03-29 17:40:02 -0400

    add component FormReferencia

---
+++ src/components/LimitePrazo/Referencias/FormReferencia.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="column">
+    <span class="subtitle">Empresa</span>
+    <div class="field">
+      <div class="label">Nome</div>
+      <input
+        type="text"
+        class="input is-small"
+        for="empresa"
+        v-model="referencia.nome"
+      />
+    </div>
+    <div class="field">
+      <div class="label">Empresa</div>
+      <input
+        type="text"
+        class="input is-small"
+        for="empresa"
+        v-model="referencia.nome"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import IReferencia from "@/interfaces/IReferencia";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "FormReferencia",
+  data() {
+    return {
+      referencia: {} as IReferencia,
+    };
+  },
+});
+</script>
