7dc2a1357a90c8d23f885fd95d38e8d733ad9a37
GUILHERME CASTELO <guilhermecastelo.mail@gmail.com>
2022-03-29 17:30:14 -0400

    add component button

---
+++ src/components/Button.vue
@@ -0,0 +1,26 @@
+<template>
+  <button class="control button" :class="tipo" @click="action">
+    <p v-show="name">{{ name }}</p>
+    <span class="icon">
+      <i class="fa" :class="icon" />
+    </span>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  emits: ["action"],
+  props: {
+    icon: {},
+    name: {},
+    tipo: {},
+  },
+  methods: {
+    action() {
+      this.$emit("action");
+    },
+  },
+});
+</script>
