46cf5fd16f497cc6919239a5459c889f7afe85c8
GUILHERME CASTELO <guilhermecastelo.mail@gmail.com>
2022-03-29 17:37:46 -0400

    refatoracao, validacao, mounted, add LimitePrazo

--- src/views/clientes/Formulario.vue
+++ src/views/clientes/Formulario.vue
@@ -1,11 +1,32 @@
 <template>
   <section class="clientes">
-    <div class="column is-10">
+    <router-link to="/clientes">
+      <Button
+        icon="fa-arrow-left"
+        tipo="is-link is-small is-outlined"
+        name="Retornar"
+        v-show="idCli"
+      />
+    </router-link>
+
+    <div class="column is-10" @change="salvaStore">
       <span class="title"> Cadastrar Cliente {{ tipoCli }} </span>
-      <button class="button is-danger" v-show="idCli" @dblclick="deleteCli">
-        DELETE
-      </button>
-      <form @submit.prevent="salvar">
+      <form @submit.prevent="salvar" class="box">
+        <div class="field is-horizontal">
+          <Button
+            icon="fa-trash"
+            tipo="is-danger is-small is-outlined"
+            v-show="idCli"
+            name="Excluir cliente"
+            @action="deleteCli"
+          />
+          <Button
+            icon="fa-pen"
+            tipo="is-link is-small is-outlined"
+            v-show="idCli"
+            name="Editar cliente"
+          />
+        </div>
         <div class="field is-horizontal">
           <label for="razaoSocial" class="label"> Razão Social </label>
           <input
@@ -22,21 +43,15 @@
             class="input"
             v-model="currentCliente.cnpjCpf"
             id="cnpjCpf"
-            @input="findByCnpjCpf(currentCliente.cnpjCpf)"
+            @input="validaCnpjCpf"
           />
         </div>
-        <p class="endereco" v-show="currentCliente.cnpjCpf">
-          <Endereco @endereco="buscaEndereco" :endereco="currentCliente" />
-        </p>
-        <p class="contato" v-show="currentCliente.cidade">
-          <Contato @contato="buscaContato" />
-        </p>
-        <p class="autorizacoes" v-show="currentCliente.emailContato">
-          <Autorizacoes @autoriza="buscaAutorizacao" />
-        </p>
-        <br />
-        <div class="field" v-show="currentCliente.autorizaPublicidade">
-          <button class="button is-link" type="submit">Salvar</button>
+        <Endereco @endereco="buscaEndereco" />
+        <Contato @contato="buscaContato" />
+        <Autorizacoes @autoriza="buscaAutorizacao" />
+        <LimitePrazo v-show="currentCliente.tipoPJF == 'PJ'" />
+        <div class="field">
+          <Button name="Salvar" tipo="is-info is-outlined" icon="fa-plus" />
         </div>
       </form>
     </div>
@@ -46,7 +61,11 @@
 <script lang="ts">
 import { defineComponent, computed } from "vue";
 import { useStore } from "@/store";
-import { ADICIONA_CLIENTE, ALTERA_CLIENTE } from "@/store/tipo-mutacao";
+import {
+  ADICIONA_CLIENTE,
+  ALTERA_CLIENTE,
+  LIMPAR_LISTA,
+} from "@/store/tipo-mutacao";
 import { tipoNotificacao } from "@/interfaces/INotificacao";
 import useNotificador from "@/hooks/notificador";
 import ICliente from "@/interfaces/ICliente";
@@ -56,6 +75,8 @@ import Contato from "@/components/Contato.vue";
 import ClienteDataService from "@/services/ClienteDataService";
 import ResponseData from "@/interfaces/ResponseData";
 import validarForm from "@/mixins/validarForm";
+import Button from "@/components/Button.vue";
+import LimitePrazo from "@/components/LimitePrazo/LimitePrazo.vue";
 
 export default defineComponent({
   name: "Formulario",
@@ -64,48 +85,26 @@ export default defineComponent({
       currentCliente: {} as ICliente,
       tipoCli: "",
       idCli: "",
+      limitePrazo: false,
     };
   },
-  props: {
-    id: {
-      type: String,
-    },
-  },
-  mounted() {
-    if (this.id) {
+  async mounted() {
+    this.store.commit(LIMPAR_LISTA);
+    this.idCli = validarForm.methods.buscaIdURL();
+    await this.searchCliByID(this.idCli);
+    if (this.idCli) {
       const cliente = this.store.state.clientes.find(
-        (cli) => cli.id == this.id
+        async (cli) => cli.id == this.idCli
       );
-
       if (cliente != null) {
         this.currentCliente.id = cliente.id;
         this.currentCliente.tipoPJF = cliente.tipoPJF;
-        this.currentCliente.numero = cliente.numero;
-        this.currentCliente.bairo = cliente.bairo;
-        this.currentCliente.cidade = cliente.cidade;
-        this.currentCliente.pontoReferencia = cliente.pontoReferencia;
-        this.currentCliente.emailNfe = cliente.emailNfe;
-        this.currentCliente.emailContato = cliente.emailContato;
-        this.currentCliente.celular = cliente.celular;
-        this.currentCliente.telefoneComercial = cliente.telefoneComercial;
         this.currentCliente.quantidadeCheckout = cliente.quantidadeCheckout;
-        this.currentCliente.autorizaDadosLgpd = cliente.autorizaDadosLgpd;
         this.currentCliente.cnpjCpf = cliente.cnpjCpf;
-        this.currentCliente.autorizaMensagens = cliente.autorizaMensagens;
-        this.currentCliente.autorizaPublicidade = cliente.autorizaPublicidade;
-        this.currentCliente.autorizaConsultaCredito =
-          cliente.autorizaConsultaCredito;
-        this.currentCliente.autorizaConsultaReferencia =
-          cliente.autorizaConsultaReferencia;
-        this.currentCliente.tipoImovel = cliente.tipoImovel;
-        this.currentCliente.referencias = cliente.referencias;
         this.currentCliente.inscricaoEstadual = cliente.inscricaoEstadual;
         this.currentCliente.razaoSocial = cliente.razaoSocial;
         this.currentCliente.nomeFanstasia = cliente.nomeFanstasia;
         this.currentCliente.ramoAtividade = cliente.ramoAtividade;
-        this.currentCliente.cep = cliente.cep;
-        this.currentCliente.estado = cliente.estado;
-        this.currentCliente.logradouro = cliente.logradouro;
       } else {
         this.notificar(
           tipoNotificacao.FALHA,
@@ -114,40 +113,41 @@ export default defineComponent({
         );
       }
     }
-    console.log(this.id);
-    
-    this.searchCliByID();
   },
   methods: {
     salvar() {
-      this.validaCpf();
+      this.validaCnpjCpf;
       if (this.idCli) {
-        this.store.commit(ALTERA_CLIENTE, {
-          id: this.currentCliente.id,
-          nome: this.currentCliente.razaoSocial,
-        });
-        ClienteDataService.update(
-          this.currentCliente.id,
-          this.currentCliente
-        ).then(
+        this.store.commit(ALTERA_CLIENTE, this.currentCliente);
+        ClienteDataService.update(this.idCli, this.currentCliente).then(
           (response: ResponseData) => {
             console.log(response);
+            this.notificar(
+              tipoNotificacao.SUCESSO,
+              "Sucesso",
+              "Cliente alterado com sucesso"
+            );
           },
           (e: Error) => {
             console.log(e);
+            this.notificar(
+              tipoNotificacao.FALHA,
+              "Fahou",
+              "Dados cliente nao foram atualizados " + e
+            );
           }
-        );
-        this.notificar(
-          tipoNotificacao.SUCESSO,
-          "Alterar",
-          "Cliente alterado com sucesso"
         );
       } else {
         if (!this.findByCnpjCpf(this.currentCliente.cnpjCpf)) {
+          this.notificar(
+            tipoNotificacao.ATENCAO,
+            "Duplicado",
+            "CPF/CNPJ " + this.currentCliente.cnpjCpf + " ja existe"
+          );
+        } else {
           ClienteDataService.create(this.currentCliente).then(
             (response: ResponseData) => {
-              console.log(response);
-              this.store.commit(ADICIONA_CLIENTE, this.currentCliente);
+              this.store.commit(ADICIONA_CLIENTE, response.data);
               this.notificar(
                 tipoNotificacao.SUCESSO,
                 "Novo",
@@ -164,26 +164,25 @@ export default defineComponent({
               );
             }
           );
-        } else {
-          this.notificar(
-            tipoNotificacao.ATENCAO,
-            "Duplicado",
-            "CPF/CNPJ " + this.currentCliente.cnpjCpf + " ja existe"
-          );
         }
       }
       console.log(this.currentCliente);
     },
+    salvaStore() {
+      this.store.commit(ADICIONA_CLIENTE, this.currentCliente);
+    },
     buscaEndereco(endereco: ICliente) {
-      this.currentCliente.cep = endereco.cep;
-      this.currentCliente.numero = endereco.numero;
-      this.currentCliente.tipoImovel = endereco.tipoImovel;
-      this.currentCliente.logradouro = endereco.logradouro;
-      this.currentCliente.bairo = endereco.bairo;
-      this.currentCliente.pontoReferencia = endereco.pontoReferencia;
-      this.currentCliente.estado = endereco.estado;
-      this.currentCliente.cidade = endereco.cidade;
-      this.currentCliente.quantidadeCheckout = "1";
+      if (!this.idCli) {
+        this.currentCliente.cep = endereco.cep;
+        this.currentCliente.numero = endereco.numero;
+        this.currentCliente.tipoImovel = endereco.tipoImovel;
+        this.currentCliente.logradouro = endereco.logradouro;
+        this.currentCliente.bairo = endereco.bairo;
+        this.currentCliente.pontoReferencia = endereco.pontoReferencia;
+        this.currentCliente.estado = endereco.estado;
+        this.currentCliente.cidade = endereco.cidade;
+        this.currentCliente.quantidadeCheckout = "1";
+      }
     },
     buscaContato(contato: ICliente) {
       this.currentCliente.celular = contato.celular;
@@ -196,15 +195,18 @@ export default defineComponent({
       this.currentCliente.autorizaMensagens = autoriza.autorizaMensagens;
       this.currentCliente.autorizaPublicidade = autoriza.autorizaPublicidade;
     },
-    searchCliByID() {      
-      this.idCli = window.location.hash.replace(/[/#clientesnovo]/g, "");
-      ClienteDataService.get(this.idCli).then((response: ResponseData) => {
-        console.log(response.data);
-        this.currentCliente = response.data;
-      });
+    async searchCliByID(idCli: string) {
+      if (idCli) {
+        await ClienteDataService.get(idCli).then(
+          async (response: ResponseData) => {
+            this.store.commit(LIMPAR_LISTA);
+            this.store.commit(ADICIONA_CLIENTE, response.data);
+          }
+        );
+      }
     },
-    async deleteCli() {
-      await ClienteDataService.delete(this.currentCliente.id).then(
+    deleteCli() {
+      ClienteDataService.delete(this.currentCliente.id).then(
         (response: ResponseData) => {
           console.log(response);
           this.notificar(
@@ -217,7 +219,7 @@ export default defineComponent({
           this.$router.push("/clientes");
         },
         (e: Error) => {
-          console.log(e.stack);
+          console.log(e);
           this.notificar(
             tipoNotificacao.FALHA,
             "ERRO",
@@ -229,17 +231,26 @@ export default defineComponent({
         }
       );
     },
-    validaCpf() {
-      !validarForm.methods.validarNulo(this.currentCliente.cnpjCpf)
-        ? (this.currentCliente.tipoPJF = validarForm.methods.validaTipoCli(
-            this.currentCliente.cnpjCpf
-          ))
-        : false;
+    validaCnpjCpf() {
+      if (!validarForm.methods.validarNulo(this.currentCliente.cnpjCpf)) {
+        this.currentCliente.tipoPJF = validarForm.methods.validaTipoCli(
+          this.currentCliente.cnpjCpf
+        );
+        if (this.currentCliente.tipoPJF == "PJ") this.limitePrazo = true;
+        this.findByCnpjCpf(this.currentCliente.cnpjCpf);
+      } else {
+        false;
+      }
     },
     findByCnpjCpf(cnpjCpf: string): boolean {
       ClienteDataService.findByCnpjCpf(cnpjCpf).then(
         (response: ResponseData) => {
-          if (response.data.content[0]) {
+          if (response.data.content.length > 0) {
+            this.notificar(
+              tipoNotificacao.FALHA,
+              "Duplicado",
+              "CPF/CNPJ " + this.currentCliente.cnpjCpf + " ja existe"
+            );
             return true;
           }
         }
@@ -256,34 +267,7 @@ export default defineComponent({
       notificar,
     };
   },
-  /* created() {
-        this.currentCliente.tipoPJF = "PF";
-        this.currentCliente.numero = "1234";
-        this.currentCliente.bairo = "Setor 05";
-        this.currentCliente.cidade = "cliente.cidade";
-        this.currentCliente.pontoReferencia = "cliente.pontoReferencia";
-        this.currentCliente.emailNfe = "cliente.emailNfe";
-        this.currentCliente.emailContato = "cliente.emailContato";
-        this.currentCliente.celular = "cliente.celular";
-        this.currentCliente.telefoneComercial = "cliente.telefoneComercial";
-        this.currentCliente.quantidadeCheckout = "cliente.quantidadeCheckout";
-        this.currentCliente.autorizaDadosLgpd = true;
-        this.currentCliente.cnpjCpf = "03936781214";
-        this.currentCliente.autorizaMensagens = true;
-        this.currentCliente.autorizaPublicidade = true;
-        this.currentCliente.autorizaConsultaCredito =true;
-        this.currentCliente.autorizaConsultaReferencia = true;
-        this.currentCliente.tipoImovel = eTipoImovel.ALUGADO;
-        this.currentCliente.referencias =  {} as IReferencia;
-        this.currentCliente.inscricaoEstadual = "cliente.inscricaoEstadual";
-        this.currentCliente.razaoSocial = "GUILHERME";
-        this.currentCliente.nomeFanstasia = "cliente.nomeFanstasia";
-        this.currentCliente.ramoAtividade ="cliente.ramoAtividade";
-        this.currentCliente.cep = "76870684";
-        this.currentCliente.estado = "cliente.estado";
-        this.currentCliente.logradouro = "cliente.logradouro";
-  }, */
-  components: { Endereco, Autorizacoes, Contato },
+  components: { Endereco, Autorizacoes, Contato, Button, LimitePrazo },
 });
 </script>
 
