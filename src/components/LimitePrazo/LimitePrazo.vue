3f2fdc132a035fea6b40586911cdcbfab16b1db3
GUILHERME CASTELO <guilhermecastelo.mail@gmail.com>
2022-03-29 17:39:46 -0400

    add componente LimitePrazo

---
+++ src/components/LimitePrazo/LimitePrazo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="columns">
+    <div class="column">
+      <span class="title is-3">Limite e Prazo</span>
+      <Documentos/>
+      <br>
+      <Referencia/>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Documentos from "./Document/Documentos.vue";
+import Referencia from "./Referencias/Referencia.vue";
+
+export default defineComponent({
+    name: "LimitePrazo",
+    data() {
+        return {};
+    },
+    components: { Documentos, Referencia }
+});
+</script>
