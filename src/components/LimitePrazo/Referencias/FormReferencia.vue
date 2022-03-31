e70dae3a82d0f22acea23a700bed5f7baadf1d7a
GUILHERME CASTELO <guilhermecastelo.mail@gmail.com>
2022-03-31 16:38:27 -0400

    componente reformulado

--- src/components/LimitePrazo/Referencias/FormReferencia.vue
+++ src/components/LimitePrazo/Referencias/FormReferencia.vue
@@ -1,37 +1,64 @@
 <template>
-  <div class="column">
-    <span class="subtitle">Empresa</span>
+  <div>
     <div class="field">
-      <div class="label">Nome</div>
       <input
         type="text"
         class="input is-small"
         for="empresa"
-        v-model="referencia.nome"
+        placeholder="Nome"
+        v-model="formRef.nome"
       />
     </div>
     <div class="field">
-      <div class="label">Empresa</div>
       <input
         type="text"
         class="input is-small"
-        for="empresa"
-        v-model="referencia.nome"
+        for="telefone"
+        placeholder="Telefone"
+        v-model="formRef.telefone"
       />
     </div>
+    <div class="field">
+      <input
+        type="email"
+        class="input is-small"
+        for="email"
+        placeholder="Email"
+        v-model="formRef.email"
+      />
+    </div>
+    <Button
+      name="Inserir"
+      tipo="is-info is-small is-outlined"
+      icon="fa-arrow-right"
+      @action="enviarDados"
+    />
   </div>
 </template>
 
 <script lang="ts">
-import IReferencia from "@/interfaces/IReferencia";
 import { defineComponent } from "vue";
+import Button from "@/components/Button.vue";
+import IReferencia from "@/interfaces/IReferencia";
 
 export default defineComponent({
   name: "FormReferencia",
   data() {
     return {
-      referencia: {} as IReferencia,
+      formRef: {} as IReferencia,
     };
   },
+  emits: ["enviar"],
+  methods: {
+    enviarDados() {
+        this.$emit("enviar", {...this.formRef});
+        this.formRef.nome = "";
+        this.formRef.telefone = "";
+        this.formRef.email = "";
+    },
+  },
+  components: { Button },
 });
 </script>
+
+<style scoped></style>
