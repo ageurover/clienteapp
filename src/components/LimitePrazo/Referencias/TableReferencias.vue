f7f4315914c6d83b9b5ed6c7e72b9485fb4dafd4
GUILHERME CASTELO <guilhermecastelo.mail@gmail.com>
2022-04-04 17:43:56 -0400

    add methods delete

--- src/components/LimitePrazo/Referencias/TableReferencias.vue
+++ src/components/LimitePrazo/Referencias/TableReferencias.vue
@@ -1,26 +1,45 @@
 <template>
   <tbody class="table-referencias table resp">
     <tr v-for="(ref, id) in refs" :key="id">
-      <div class="columns">
-        <div id="icon" class="column">
-          <i class="fas fa-address-card" />
+      <article class="message is-info">
+        <div class="message-header">
+          <div id="icon" class="column">
+            <i class="fas fa-address-card" />
+          </div>
+          {{ ref.nome }}
+          <button class="delete" @click="deleteRef(ref)"></button>
         </div>
-        <div class="column is-four-fifths">
-          <td class="tag">{{ ref.nome }}</td>
-          <td class="tag">{{ ref.telefone }}</td>
-          <td class="tag">{{ ref.email }}</td>
+        <div class="message-body is-small">
+          {{ ref.telefone }}
+          {{ ref.email }}
         </div>
-      </div>
+      </article>
     </tr>
   </tbody>
 </template>
 
 <script lang="ts">
+import IReferencia from "@/interfaces/IReferencia";
 import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "TableReferencuias",
+  data() {
+    return {
+      referencias: [] as IReferencia[],
+    };
+  },
   props: ["refs"],
+  methods: {
+    deleteRef(ref: IReferencia) {
+      this.referencias = this.refs;
+      var indice = this.referencias.indexOf(ref);
+      while (indice >= 0) {
+        this.referencias.splice(indice, 1);
+        indice = this.referencias.indexOf(ref);
+      }
+    },
+  },
 });
 </script>
 
