622115c44e72a5006c37921cbc8d9fd93d2fa3a4
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2022-08-04 10:21:38 -0400

    feat: add footer

---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,45 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <div class="title is-1">
+        <span class="icon-text is-responsive">
+          <span>Viva a</span>
+          <span class="icon has-text-success">
+            <i class="fa fa-heart "></i>
+          </span>
+          <span>Economia!</span>
+        </span>
+      </div>
+      <div>
+        <span class="icon-text is-responsive">
+          <span class="icon has-text-black">
+            <i class="fa fa-copyright"></i>
+          </span>
+          <span>
+            Copyright
+            <strong>
+              <a href="https://brasilsuperatacado.com.br/">
+                Brasil Super Atacado
+              </a>
+            </strong>
+            2022 | Todos os Direitos Reservados | Feito por
+            <a href="https://linkedin.com/in/castelo-guilherme"
+              >Guilherme Castelo</a
+            >.
+          </span>
+        </span>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style lang="css">
+.title {
+  font-weight: 700;
+}
+.footer {
+  background-color: rgb(224, 224, 224);
+  flex-shrink: 0;
+  width: 100%;
+}
+</style>
