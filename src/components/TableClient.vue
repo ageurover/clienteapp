3c439bc31cd63e5d02805371ff60091658bc6baf
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2022-08-04 10:20:59 -0400

    recactor: add class column and validation clientes

--- src/components/TableClient.vue
+++ src/components/TableClient.vue
@@ -1,51 +1,59 @@
 <template>
-  <table class="table is-responsive">
-    <thead>
-      <tr>
-        <th>id</th>
-        <th>Cnpj/Cpf</th>
-        <th>Nome</th>
-        <th>Telefone</th>
-        <th>Celular</th>
-        <th>Ações</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="cliente in clientes" :key="cliente.id">
-        <td>{{ cliente.id }}</td>
-        <td>{{ cliente.tipoPJF }} - {{ cliente.cnpjCpf }}</td>
-        <td>{{ cliente.razaoSocial }}</td>
-        <td>{{ cliente.telefoneComercial }}</td>
-        <td>{{ cliente.celular }}</td>
-        <td>
-          <span :v-show="cliente.tipoPJF == tipoCli">
-            <router-link :to="`/clientes/cadastro/${cliente.id}`" >
-              <button class="button is-info is-small is-responsive">
-                <i class="fa fa-pencil-alt "></i>
-              </button>
-            </router-link>
-          </span>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+  <div class="column is-10 is-offset-1">
+    <table
+      class="table is-striped is-narrow is-hoverable is-fullwidth is-responsive is-centered"
+    >
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>Cnpj/Cpf</th>
+          <th>Nome</th>
+          <th>Telefone</th>
+          <th>Celular</th>
+          <th>Ações</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-if="!clientes">
+          <td class="is-light">------</td>
+          <td class="is-light">------</td>
+          <td class="is-light">------</td>
+          <td class="is-light">------</td>
+          <td class="is-light">------</td>
+          <td class="is-light">------</td>
+        </tr>
+        <tr v-for="cliente in clientes" :key="cliente.id">
+          <td>{{ cliente.id }}</td>
+          <td>{{ cliente.tipoPJF }} - {{ cliente.cnpjCpf }}</td>
+          <td>{{ cliente.razaoSocial }}</td>
+          <td>{{ cliente.telefoneComercial }}</td>
+          <td>{{ cliente.celular }}</td>
+          <td>
+            <span :v-show="cliente.tipoPJF == tipoCli">
+              <router-link :to="`/cadastro/${cliente.id}`">
+                <button class="button is-info is-small is-responsive">
+                  <i class="fa fa-pencil-alt "></i>
+                </button>
+              </router-link>
+            </span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
-    props: {
-        clientes: {
-            type: Object,
-        },
-        tipoCli: {
-            type: String,
-        },
+  props: {
+    clientes: {
+      type: Object,
     },
+    tipoCli: {
+      type: String,
+    },
+  },
 });
 </script>
-
-<style>
-@import url("../assets/listaStyle.css");
-</style>
