8431833842afa0e6ce213e3fff6bec15885b5841
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2022-08-04 10:43:04 -0400

    feat: limpar store e montar tabs

--- src/views/clientes/Cadastro.vue
+++ src/views/clientes/Cadastro.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="clientes">
-    <router-link to="/clientes">
-      <button class="button is-inverted is-link">
+    <router-link to="/">
+      <button @click="limpaClientes" class="button is-inverted is-link">
         <span class="icon">
           <i class="fa fa-arrow-left"></i>
         </span>
@@ -18,12 +18,36 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { computed, defineComponent } from "vue";
 import Formulario from "@/components/Formulario/Formulario.vue";
+import { useStore } from "@/store";
+import {
+  DEFINIR_TABS,
+  LIMPAR_LISTA_CLI,
+  LIMPAR_LISTA_DOCS,
+  LIMPAR_LISTA_REFS,
+} from "@/store/tipo-acoes";
 
 export default defineComponent({
   name: "Cadastro",
   components: { Formulario },
+  setup() {
+    const store = useStore();
+    return {
+      tabs: computed(() => store.state.tab.tabs),
+      store,
+    };
+  },
+  mounted() {
+    this.store.dispatch(DEFINIR_TABS, this.tabs);
+  },
+  methods: {
+    limpaClientes() {
+      this.store.dispatch(LIMPAR_LISTA_CLI);
+      this.store.dispatch(LIMPAR_LISTA_REFS);
+      this.store.dispatch(LIMPAR_LISTA_DOCS);
+    },
+  },
 });
 </script>
 
