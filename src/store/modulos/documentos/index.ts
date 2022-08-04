20539fd2a3026bfcb03ba303d413c0707c0e8163
Guilherme Castelo <guilhermecastelo.mail@gmail.com>
2022-08-04 10:50:18 -0400

    feat: action limpar lista docs

--- src/store/modulos/documentos/index.ts
+++ src/store/modulos/documentos/index.ts
@@ -1,9 +1,19 @@
 import { Module } from "vuex";
-import { ADICIONA_DOCUMENTO, DEFINE_DOCUMENTOS, LIMPA_LISTA_DOCS, REMOVE_DOCUMENTO } from "../../tipo-mutacao";
+import {
+  ADICIONA_DOCUMENTO,
+  DEFINE_DOCUMENTOS,
+  LIMPA_LISTA_DOCS,
+  REMOVE_DOCUMENTO,
+} from "../../tipo-mutacao";
 
 import IDocumento from "@/interfaces/IDocumento";
 import { Estado } from "@/store";
-import { ADICIONAR_DOCUMENTO, DEFINIR_DOCUMENTOS, REMOVER_DOCUMENTO } from "@/store/tipo-acoes";
+import {
+  ADICIONAR_DOCUMENTO,
+  DEFINIR_DOCUMENTOS,
+  LIMPAR_LISTA_DOCS,
+  REMOVER_DOCUMENTO,
+} from "@/store/tipo-acoes";
 
 export interface EstadoDocumento {
   documentos: IDocumento[];
@@ -17,22 +27,25 @@ export const documento: Module<EstadoDocumento, Estado> = {
     [DEFINE_DOCUMENTOS](state, docs: IDocumento[]): void {
       state.documentos = docs;
     },
-    [ADICIONA_DOCUMENTO](state, doc: IDocumento): void{
-      state.documentos.push(doc)
+    [ADICIONA_DOCUMENTO](state, doc: IDocumento): void {
+      state.documentos.push(doc);
     },
-    [REMOVE_DOCUMENTO](state, doc: IDocumento): void{
-      state.documentos = state.documentos.filter((d)=> d != doc)
-    }
+    [REMOVE_DOCUMENTO](state, doc: IDocumento): void {
+      state.documentos = state.documentos.filter((d) => d != doc);
+    },
   },
   actions: {
-    async [DEFINIR_DOCUMENTOS]({ commit }, documentos: IDocumento[]) {     
+    async [DEFINIR_DOCUMENTOS]({ commit }, documentos: IDocumento[]) {
       commit(DEFINE_DOCUMENTOS, documentos);
     },
-    [ADICIONAR_DOCUMENTO]({commit}, documento: IDocumento){
-      commit(ADICIONA_DOCUMENTO, documento)
+    [ADICIONAR_DOCUMENTO]({ commit }, documento: IDocumento) {
+      commit(ADICIONA_DOCUMENTO, documento);
     },
-    [REMOVER_DOCUMENTO]({commit}, documento: IDocumento){
-      commit(REMOVE_DOCUMENTO, documento)
-    }
+    [REMOVER_DOCUMENTO]({ commit }, documento: IDocumento) {
+      commit(REMOVE_DOCUMENTO, documento);
+    },
+    [LIMPAR_LISTA_DOCS]({ commit }) {
+      commit(LIMPA_LISTA_DOCS);
+    },
   },
 };
